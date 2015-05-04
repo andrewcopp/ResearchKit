a6fef4972bf9ab7747923c3db6e9b0d80bd479b5
Andrew Copp <andrew.w.copp@gmail.com>
2015-05-04 17:24:53 -0400

    more boilerplate as i figure out what is required to be in an active step and active step view controller subclasses

--- ResearchKit/ActiveTasks/ORKVisualAcuityStep.m
+++ ResearchKit/ActiveTasks/ORKVisualAcuityStep.m
@@ -8,7 +8,56 @@
 
 #import "ORKVisualAcuityStep.h"
 #import "ORKVisualAcuityStepViewController.h"
+#import "ORKHelpers.h"
+#import "ORKStep_Private.h"
 
 @implementation ORKVisualAcuityStep
+
++ (Class)stepViewControllerClass
+{
+    return [ORKVisualAcuityStepViewController class];
+}
+
+- (instancetype)initWithIdentifier:(NSString *)identifier
+{
+    self = [super initWithIdentifier:identifier];
+    return self;
+}
+
+- (id)copyWithZone:(NSZone *)zone
+{
+    ORKVisualAcuityStep *step = [super copyWithZone:zone];
+    
+    return step;
+}
+
+- (void)validateParameters
+{
+    [super validateParameters];
+}
+
+- (instancetype)initWithCoder:(NSCoder *)aDecoder
+{
+    self = [super initWithCoder:aDecoder];
+    if (self) {
+        //
+    }
+    return self;
+}
+
+- (void)encodeWithCoder:(NSCoder *)aCoder
+{
+    [super encodeWithCoder:aCoder];
+}
+
++ (BOOL)supportsSecureCoding
+{
+    return YES;
+}
+
+- (BOOL)allowsBackNavigation
+{
+    return NO;
+}
 
 @end
--- ResearchKit/ActiveTasks/ORKVisualAcuityStepViewController.m
+++ ResearchKit/ActiveTasks/ORKVisualAcuityStepViewController.m
@@ -14,24 +14,49 @@
 
 @implementation ORKVisualAcuityStepViewController
 
-- (void)viewDidLoad {
+- (instancetype)initWithStep:(ORKStep *)step
+{
+    self = [super initWithStep:step];
+    if (self) {
+        //
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (void)viewDidAppear:(BOOL)animated
+{
+    [super viewDidAppear:animated];
+    [self start];
 }
 
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
+- (void)start
+{
+    [super start];
 }
-*/
+
+- (void)suspend
+{
+    [super suspend];
+}
+
+- (void)resume
+{
+    [super resume];
+}
+
+- (void)finish
+{
+    [super finish];
+}
+
+- (void)stepDidFinish
+{
+    
+}
 
 @end
