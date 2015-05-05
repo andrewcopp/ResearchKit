74e2c76816acb9730fa5edac1e72a1cac4736de0
Andrew Copp <andrew.w.copp@gmail.com>
2015-05-04 23:46:02 -0400

    broke ground on a new recorder type that will use face detection to detect distance from the screen

---
+++ ResearchKit/ActiveTasks/ORKDistanceRecorder.m
@@ -0,0 +1,14 @@
+//
+//  ORKDistanceRecorder.m
+//  ResearchKit
+//
+//  Created by Andrew Copp on 5/4/15.
+//  Copyright (c) 2015 researchkit.org. All rights reserved.
+//
+
+#import "ORKDistanceRecorder.h"
+#import <CoreImage/CoreImage.h>
+
+@implementation ORKDistanceRecorder
+
+@end
